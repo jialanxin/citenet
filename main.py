66897c9b370ceb03a83b719f0f18b9bd42913646
jialanxin <jialanxin1996@hotmail.com>
2020-05-15 02:51:11 +0800

    大幅减少正则使用，改用文件行数和行开头进行筛选

--- main.py
+++ main.py
@@ -3,37 +3,75 @@ with open("savedrecs.txt","r") as f:
     chunks = re.split(r'\n(?=PT J)',f.read(),flags=re.MULTILINE)
 
 class Article(object):
-    def __init__(self,doi,ref_doi_list,tc):
+    def __init__(self,doi,ref_list,tc,ti):
         self.doi =doi
-        self.ref_doi_list = ref_doi_list
-        self.cr = len(ref_doi_list)
+        self.ref_list = ref_list
+        self.cr = len(ref_list)
         self.gcs = tc
         self.lcs = 0
         self.lcr = 0
-        self.lcs_doi_list = []
+        self.lcs_list = []
+        self.title = ti
 
 local_article_list = []
 
 for each in chunks[1:]:
-    self_doi = re.split(r' ',re.findall(r'DI \d{2}\.\d{4}/.*\n',each)[0])[1]
-    self_tc  = int(re.split(r' ',re.findall(r'TC \d*\n',each)[0])[1])
-    cr_list = re.findall(r'DOI \[?\d{2}\.\d{4}/.*\n',each)
-    ref_doi_list = []
-    for cr in cr_list:
-        ref_doi_list.append(re.findall(r'\d{2}\.\d{4}/.*?\s',cr)[0].replace(',','\n'))
-    local_article_list.append(Article(self_doi,ref_doi_list,self_tc))
+    split_lines = each.splitlines()
+    for line_num in range(len(split_lines)):
+        if re.match(r'TI ',split_lines[line_num]) != None:
+            TI_line_num = line_num
+        if re.match(r'SO ',split_lines[line_num]) != None:
+            SO_line_num = line_num
+        if re.match(r'CR ',split_lines[line_num]) != None:
+            CR_line_num = line_num
+        if re.match(r'NR ',split_lines[line_num]) != None:
+            NR_line_num = line_num
+        if re.match(r'TC ',split_lines[line_num]) != None:
+            TC_line_num = line_num
+        if re.match(r'DI ',split_lines[line_num]) != None:
+            DI_line_num = line_num
+
+    TI_lines = split_lines[TI_line_num:SO_line_num]
+    TI = ' '.join(TI_lines).replace('    ',' ').lstrip('TI ')
+
+    TC_line = split_lines[TC_line_num]
+    TC = int(TC_line.lstrip('TC '))
+
+    DI_line = split_lines[DI_line_num]
+    DI = DI_line.lstrip('DI ')
+
+    CR_lines = split_lines[CR_line_num:NR_line_num]
+    ref_list = []
+    for line_num in range(len(CR_lines)):
+        if line_num == 0:
+            CR_lines[line_num] = CR_lines[line_num].lstrip('CR ')
+        else:
+            CR_lines[line_num] = CR_lines[line_num].lstrip(' ')
+        if len(CR_lines[line_num].split(', DOI ')) != 2:
+            continue
+        ref_title, doi = CR_lines[line_num].split(', DOI ')[0],CR_lines[line_num].split(', DOI ')[1]
+        if doi[0] == '[':
+            doi = doi.split(',')[0].lstrip('[')
+        ref = {"ref_title":ref_title,"ref_doi":doi}
+        ref_list.append(ref)   
+    local_article_list.append(Article(DI,ref_list,TC,TI))
 
 for article_use in local_article_list:
-    for ref_doi in article_use.ref_doi_list:
-        for article_source in local_article_list:
-            if ref_doi == article_source.doi:
+    for article_source in local_article_list:
+        for ref in article_use.ref_list:
+            if ref["ref_doi"] == article_source.doi:
                 article_use.lcr += 1
-                article_source.lcs_doi_list.append(ref_doi)
                 article_source.lcs += 1
+                article_source.lcs_list.append(ref)
 
 for article in local_article_list:
-    if article.lcs != 0:
-        print(article.doi ,article.lcs_doi_list)
+    print(article.lcs_list)
+
+
+
+
+
+
 
 
 
